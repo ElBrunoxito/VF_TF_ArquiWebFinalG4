7c1b0f53028923f3e5560421cffd9c54555029c6
ElBrunoxito <brunoxaguilare@gmail.com>
2024-04-29 20:30:35 -0500

    primer commit :)

---
+++ src/test/java/com/grupo4/TrabajoFinalArquiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoFinalArquiWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
